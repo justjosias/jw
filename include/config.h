0f853d891d08c342172c015f5fc4447935510e49
Josias <me@josias.dev>
2020-10-13 14:18:12 +0200

    Clean up headers

--- include/config.h
+++ include/config.h
@@ -1,14 +1,6 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#define VERSION "0.1.0"
-// the maximum length of the text extracted from the body for a title
-#define FIRST_TEXT_LEN 30
-
-// the cog directory relative to home
-#define CONFIG_POST_DIR "jw"
-// cog list relative to cog dir
-#define CACHE_LIST_PATH "posts.txt"
 
 char *config_dir_get(const char*);
 
--- include/main.h
+++ include/main.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
-#include "utils.h"
+#include "utils.h" // for date
+
+#define VERSION "0.1.0"
+// the maximum length of the text extracted from the body for a title
+#define FIRST_TEXT_LEN 30
+
+// the cog directory relative to home
+#define CONFIG_POST_DIR "jw"
+// cog list relative to cog dir
+#define CACHE_LIST_PATH "posts.txt"
 
 typedef struct metadata {
 	date date;
