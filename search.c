9ab75987c7d36a3f42a56baef97a05cb2025d287
Josias <me@josias.dev>
2020-10-13 14:20:57 +0200

    Update search to use new cache scheme

--- search.c
+++ search.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#include <tinydir.h>
-
+#include "cache.h"
 #include "config.h"
 #include "search.h"
 #include "notebook.h"
@@ -21,57 +21,54 @@ struct result *search(struct notebook notebook, const char query[100], size_t *r
 	static struct result results[1000];
 	size_t result_index = 0;
 
-	tinydir_dir dir;
-	tinydir_open(&dir, notebook.path);
+	char path[512];
+	FILE *cache_file = fopen(cache_path(notebook.id), "r");
+	if (cache_file == NULL)
+		return NULL;
+	while (fgets(path, 512, cache_file)) {
+		path[strlen(path) - 1] = '\0'; // remove \n
+		if (strcmp(path, "") == 0)
+			continue;
 
-	while (dir.has_next) {
-		tinydir_file file;
-		tinydir_readfile(&dir, &file);
+		char filepath[256];
+		strncpy(filepath, notebook.path, 255);
+		strncat(filepath, path, 255);
+		filepath[255] = '\0';
 
-		if (!file.is_dir) {
-			char filepath[256];
-			strncpy(filepath, notebook.path, 255);
-			strncat(filepath, file.name, 255);
-			filepath[255] = '\0';
+		FILE *fp = fopen(filepath, "r");
 
-			FILE *fp = fopen(filepath, "r");
-
-			if (fp == NULL) {
-				continue;
-			}
-
-			int c;
-			size_t count = 0;
-			while ((c = getc(fp)) != EOF) {
-				count++;
-			}
-
-			char *contents = malloc(count);
-			size_t i = 0;
-			fseek(fp, 0, SEEK_SET);
-			while ((c = getc(fp)) != EOF && i < count) {
-				contents[i] = c;
-				i++;
-			}
-			contents[i] = '\0';
-
-			if (strstr(contents, query) != NULL) {
-				struct result r;
-				strncpy(r.path, notebook.path, 255);
-				strncat(r.path, file.name, 255);
-				r.path[255] = '\0';
-
-				results[result_index] = r;
-				result_index++;
-			}
-
-			fclose(fp);
+		if (fp == NULL) {
+			puts("No such thing");
+			continue;
 		}
 
-		tinydir_next(&dir);
-	}
+		int c;
+		size_t count = 0;
+		while ((c = getc(fp)) != EOF) {
+			count++;
+		}
 
-	tinydir_close(&dir);
+		char *contents = malloc(count);
+		size_t i = 0;
+		fseek(fp, 0, SEEK_SET);
+		while ((c = getc(fp)) != EOF && i < count) {
+			contents[i] = c;
+			i++;
+		}
+		contents[i] = '\0';
+
+		if (strstr(contents, query) != NULL) {
+			struct result r;
+			strncpy(r.path, notebook.path, 255);
+			strncat(r.path, path, 255);
+			r.path[255] = '\0';
+
+			results[result_index] = r;
+			result_index++;
+		}
+
+		fclose(fp);
+	}
 
 	*result_count = result_index;
 	return results;
